a119b174de5cb9585d9949a0355797c64b8b8310
Zhen Yi Lim <zhen.yi@outlook.com>
2025-04-30 21:48:51 +0000

    chore: replace conint with Annotated, add type annotations to Fields

--- overhaul/CanuckBot/types.py
+++ overhaul/CanuckBot/types.py
@@ -1,10 +1,10 @@
-from pydantic import Field, conint
+from pydantic import Annotated, Field
 
-Handle = Field(
+Handle: str = Field(
     ..., pattern=r"^[a-zA-Z0-9\-]+$", max_length=24
 )  # max_length=23 ensures under 24 characters
-HexColor = Field(..., pattern=r"^#(?:[0-9a-fA-F]{3}){1,2}$")
+HexColor: str = Field(..., pattern=r"^#(?:[0-9a-fA-F]{3}){1,2}$")
 # we'll need to store snowflakeid as TEXT into sqlite
-UnixTimestamp = conint(ge=0)
-SnowflakeId = conint(ge=0, le=2**64 - 1)
-DiscordChannelName = Field(..., pattern=r"^[a-z0-9-]{1,100}$")
+UnixTimestamp: int = Annotated[int, Field(strict=True, gt=0)]
+SnowflakeId: int = Annotated[int, Field(strict=True, gt=0, lt=2**64 - 1)]
+DiscordChannelName: str = Field(..., pattern=r"^[a-z0-9-]{1,100}$")
