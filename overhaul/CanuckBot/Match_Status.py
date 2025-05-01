796d2676382b084402a8205a6eba7a81c09bf6b7
saturnin55 <legare@gmail.com>
2025-05-01 10:24:06 -0400

    Changed Match_Status names

--- overhaul/CanuckBot/Match_Status.py
+++ overhaul/CanuckBot/Match_Status.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 
 
 class Match_Status(IntEnum):
-    Pending = 0
-    Active = 1
-    Completed = 2
+    Pending = 0     # Match in scheduled but not yet reached match's hours_before_kickoff
+    Ready = 1       # We reached match's hours_before_kickoff, channel should be created
+    Active = 2      # Match in progress
+    Completed = 3   # Match reached match's hours_after_kickoff, time to cleanup
