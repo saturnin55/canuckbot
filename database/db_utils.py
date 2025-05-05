52b13f847eaf492271772b3a5d75faa023d92b2b
saturnin55 <legare@gmail.com>
2025-05-05 10:15:42 -0400

    forgot to use DatabaseManager object in is_user_manager() db_utils.py

--- database/db_utils.py
+++ database/db_utils.py
@@ -4,33 +4,14 @@ from pathlib import Path
 from CanuckBot.types import User_Level, DiscordUserId
 from database import DatabaseManager
 
-
-#def connect_db():
-#    db_path = os.getenv("DB_PATH")
-#    if not db_path:
-#        raise EnvironmentError(
-#            "DB_PATH environment variable is not set. Check your .env file."
-#        )
-#
-#    path = Path(db_path)
-#    if not path.exists():
-#        raise FileNotFoundError(f"Database file not found at: {db_path}")
-#
-#    return sqlite3.connect(db_path)
-
-
-def is_user_manager(db: DatabaseManager = None, user_id: DiscordUserId = None, level: User_Level = User_Level.Superadmin):
-    # fixme need to convert this to aiosqlite and use the DatabaseManager
+async def is_user_manager(db: DatabaseManager = None, user_id: DiscordUserId = None, level: User_Level = User_Level.Superadmin):
     try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT user_id FROM managers WHERE user_id = ? AND access_level <= ?", (user_id, level.value))
-        result = cursor.fetchone()
-        conn.close()
+        row = await db.get_one("SELECT user_id FROM managers WHERE user_id = ? AND access_level <= ?", [user_id, level.value])
 
-        if result is not None:
-            return result
-        else:
+        if not row:
             return False
+        else:
+            return row
+
     except sqlite3.Error as e:
         print(f"{e}")
